fe8e9a2e5dbd4fdefc21a358f5106b9aed5c58f8
Marshall Galbraith <galbramc@mit.edu>
2014-08-25 21:41:23 -0400

    Fixed the jenkins_env.sh

--- src/Externals/jenkins/cmake_jenkins.sh
+++ src/Externals/jenkins/cmake_jenkins.sh
@@ -4,4 +4,5 @@ WORKSPACE=$(git rev-parse --show-toplevel)
 
 source $WORKSPACE/src/Externals/jenkins/jenkins_env.sh
 
-cmake $WORKSPACE/src
+cmake -DCUDA_TOOLKIT_ROOT_DIR=$CUDA_TOOLKIT_ROOT_DIR
+      $WORKSPACE/src
