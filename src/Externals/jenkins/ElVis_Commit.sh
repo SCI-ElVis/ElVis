2c02468b5896f5657dae3e8ef0c1dc3c192f5c6b
Marshall Galbraith <galbramc@mit.edu>
2014-08-30 10:10:57 -0400

    Different displays for differnt jenkins builds

--- src/Externals/jenkins/ElVis_Commit.sh
+++ src/Externals/jenkins/ElVis_Commit.sh
@@ -13,18 +13,25 @@ source $WORKSPACE/src/Externals/jenkins/cmake_jenkins.sh
 make
 make unit_build
 
+if [[ $builddir == "debug" ]]; then
+  DNUM=42
+elif [[ $builddir == "release" ]]; then
+  DNUM=52
+elif [[ $builddir == "coverage" ]]; then
+  DNUM=62
+fi
+
 #Qt testing requires an X-server to work. 
 #Xvfb :42 -ac -screen 1920x1200  &
-Xvfb :42 -ac &
+export DISPLAY=:$DNUM
+Xvfb :$DNUM -ac > /dev/null 2>&1 &
 sleep 5
-export DISPLAY=:42
-#icewm >/dev/null 2>&1 &
-icewm  &
+icewm >/dev/null 2>&1 &
 sleep 5
 
 make check
 
-kill `cat /tmp/.X42-lock`
+kill `cat /tmp/.X${DNUM}-lock`
 
 if [[ $builddir == *"coverage"* ]]; then
   make coverage_info
