d70dd2c490b6346a00828ea875318283f2d0de66
Marshall Galbraith <galbramc@mit.edu>
2014-09-03 23:20:25 -0400

    Fixed up the nightly jenkins script

--- src/Externals/jenkins/ElVis_Nightly.sh
+++ src/Externals/jenkins/ElVis_Nightly.sh
@@ -13,10 +13,21 @@ source $WORKSPACE/scripts/jenkins/jenkins_env.sh
 cmake -DVALGRIND_EXTRA_FLAGS="--track-origins=yes;--xml=yes;--xml-file=$cmakedir/unit/valgrind.%p.memcheck.xml" \
       $WORKSPACE
 
+if [[ $builddir == "debug" ]]; then
+  DNUM=42
+elif [[ $builddir == "release" ]]; then
+  DNUM=52
+elif [[ $builddir == "coverage" ]]; then
+  DNUM=62
+fi
+
 #Qt testing requires an X-server to work. 
-Xephyr :42 -ac -screen 1920x1200 >/dev/null 2>&1 &
+#Xvfb :42 -ac -screen 1920x1200  &
+export DISPLAY=:$DNUM
+Xvfb :$DNUM -ac > /dev/null 2>&1 &
 sleep 5
-DISPLAY=:42 icewm >/dev/null 2>&1 &
+icewm >/dev/null 2>&1 &
+sleep 5
 
 #This will check both dynamic and static memory
-DISPLAY=:42 make MemAndStackCheck CTESTARGS="-T Test"
+make MemAndStackCheck CTESTARGS="-T Test"
