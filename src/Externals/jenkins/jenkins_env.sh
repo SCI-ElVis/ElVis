67f7626b86c3c4d356006479c26fd45ae1f48530
GPayne <payne.gene@gmail.com>
2015-03-20 16:14:14 -0600

    Changed to new hostname for Mac node

--- src/Externals/jenkins/jenkins_env.sh
+++ src/Externals/jenkins/jenkins_env.sh
@@ -15,7 +15,7 @@ elif [[ `hostname` == "oci-ubuntu"* ]]; then
   export OptiX_INSTALL_DIR=/usr/local/NVIDIA-OptiX-SDK-3.6.0-linux64
   export CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-6.0/
 
-elif [[ `hostname` == "dhcp227"* ]]; then
+elif [[ `hostname` == "elvis"* ]]; then
 
   export OptiX_INSTALL_DIR=/Developer/OptiX
   export CUDA_TOOLKIT_ROOT_DIR=/Developer/NVIDIA/CUDA-6.0
